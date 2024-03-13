3ee1e7699701c7e761a93d630d8cc31662ffa3c8
javier.torreantunez@jgvaldemora.org <javier.torreantunez@jgvaldemora.org>
2024-03-13 13:08:19 +0100

    Añado clase Principal

---
+++ ProbandoGIT/src/paquete1/Principal.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class Principal {
+    
+    public static void main (String[] args) {
+        System.out.println ("Estamos haciendo control de versiones con Git");
+        //System.out.println ("Escribo otra línea para hacer un 2º commit");
+        //System.out.println ("Escribo otra línea para hacer commit remoto");
+    }
+}
